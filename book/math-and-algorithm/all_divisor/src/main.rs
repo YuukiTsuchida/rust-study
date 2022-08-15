be57c8040e101e673ea247e631146afc7115b29f
Yuuki Tsuchida <dev.yuukitsuchida@gmail.com>
2022-08-15 15:35:51 +0900

    素因数分解のバグ修正

--- book/math-and-algorithm/all_divisor/src/main.rs
+++ book/math-and-algorithm/all_divisor/src/main.rs
@@ -11,20 +11,20 @@ fn main() {
 }
 
 fn all_divisor(n: i64) -> Vec<i64> {
-    let mut index = 1;
+    let mut target_value = n;
+    let mut index = 2;
     let mut result: Vec<i64> = vec![];
-    while index * index <= n {
-        if n % index != 0 {
-            index += 1;
-            continue;
-        }
-
-        result.push(index);
-        if index != n / index {
-            result.push(n / index);
+    while index * index <= target_value {
+        if target_value % index == 0 {
+            target_value /= index;
+            result.push(index);
         }
 
         index += 1;
+    }
+
+    if target_value > 2 {
+        result.push(target_value);
     }
 
     result.sort();
@@ -36,6 +36,6 @@ mod test {
     #[test]
     fn get_all_divisor() {
         let v = crate::all_divisor(100);
-        assert_eq!(v, vec![1, 2, 4, 5, 10, 20, 25, 50, 100]);
+        assert_eq!(v, vec![2, 5, 10]);
     }
 }
