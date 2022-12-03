37a363ebbaf1623e9f90ddb7ef632bb13031904c
Yuuki Tsuchida <dev.yuukitsuchida@gmail.com>
2022-12-03 10:43:01 +0900

    add tauri test

--- tauri-example/src-tauri/src/main.rs
+++ tauri-example/src-tauri/src/main.rs
@@ -3,15 +3,139 @@
     windows_subsystem = "windows"
 )]
 
+use std::sync::atomic;
+use tauri::Manager;
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn test(name: &str, age: i32) -> String {
+    println!("called test function name({}), age({})", name, age);
+    format!("execute test function from {}", name)
+}
+
+#[tauri::command]
+fn test_return_result_err() -> Result<String, String> {
+    Err("failed".into())
+}
+
+#[tauri::command]
+fn test_return_result_ok() -> Result<String, String> {
+    Ok("success".into())
+}
+
+#[tauri::command]
+async fn async_command() -> String {
+    let result = other_async_function().await;
+    result
+}
+
+async fn other_async_function() -> String {
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    format!("est")
+}
+
+#[tauri::command]
+fn receive_window_object(window: tauri::Window) {
+    if window.is_devtools_open() {
+        window.close_devtools();
+    } else {
+        window.open_devtools();
+    }
+}
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  message: String,
+}
+
+#[tauri::command]
+fn receive_app_object(app: tauri::AppHandle){
+    // app.emit_all("test-event", Payload { message: "Tauri is awesome!".into() }).unwrap();
+    app.get_window("main").unwrap().emit("test-event", Payload { message: "Tauri is awesome!".into() }).unwrap();
+}
+
+struct StateValue(atomic::AtomicI32);
+
+#[tauri::command]
+fn use_state(state_value: tauri::State<StateValue>) {
+    let mut value = state_value.0.load(atomic::Ordering::SeqCst);
+    println!(
+        "state value is {}",
+        value
+    );
+    value += 1;
+    state_value.0.store(value, atomic::Ordering::SeqCst);
+}
+
 fn main() {
+    let submenu = Submenu::new("File", Menu::new()
+                .add_item(CustomMenuItem::new("quit".to_string(), "Quit"))
+                .add_item(CustomMenuItem::new("close".to_string(), "Close"))
+                .add_native_item(MenuItem::Cut));
+    let submenu2 = Submenu::new("File2", Menu::new()
+                .add_item(CustomMenuItem::new("quit".to_string(), "Quit"))
+                .add_item(CustomMenuItem::new("close".to_string(), "Close"))
+                .add_native_item(MenuItem::Cut)
+                .add_native_item(MenuItem::Minimize));
+    
+    let menu = Menu::new()
+        .add_submenu(submenu)
+        .add_submenu(submenu2);
+    // let menu = Menu::os_default("app_name");
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+            }
+
+            app.listen_global("test-event", |event| {
+                println!("test-name with payload {:?}", event.payload());
+            });
+
+            // app.once_global("test-event", |event| {
+            //     println!("once-global with {:?}", event.payload());
+            // });
+            // app.once_global(event, handler)
+
+            let window = app.get_window("main").unwrap();
+            let id = window.listen("window-event", |event| {
+                println!("window event with payload {:?}", event.payload());
+            });
+
+            Ok(())
+        })
+        .menu(menu)
+        .on_menu_event(|event|{
+            match event. menu_item_id() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                "close" => {
+                    event.window().close().unwrap();
+                }
+                _ => { }
+            }
+        })
+        .manage(StateValue(atomic::AtomicI32::new(42)))
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            test,
+            test_return_result_err,
+            test_return_result_ok,
+            async_command,
+            receive_window_object,
+            use_state,
+            receive_app_object
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- tauri-example/src/pages/index.tsx
+++ tauri-example/src/pages/index.tsx
@@ -5,6 +5,11 @@ import reactLogo from "../assets/react.svg";
 import tauriLogo from "../assets/tauri.svg";
 import nextLogo from "../assets/next.svg";
 
+import { emit, listen } from '@tauri-apps/api/event';
+import { appWindow } from "@tauri-apps/api/window";
+// import * as win from "@tauri-apps/api/window";
+
+
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
@@ -12,44 +17,75 @@ function App() {
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name }));
+    test();
+
+    let t = await listen("test-event", event => {
+      console.log(event);
+    });
+
+    // await listen("window-event", event => {
+    //     console.log(`window event: ${event}`);
+    // });
   }
 
-  return (
-    <div className="container">
+  async function test() {
+    const str = await invoke("test", { name: "test", age: 35 });
+    console.log(str);
+  }
+
+  async function resultTest(functionName) {
+    invoke(functionName)
+      .then(message => console.log(message))
+      .catch(error => console.error(error));
+
+    try {
+      const result = await invoke(functionName);
+      console.log(result);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async function callAsyncFunction() {
+    const t = await invoke("async_command");
+    console.log(t)
+  }
+
+  async function passWindow() {
+    invoke("receive_window_object");
+  }
+
+  async function passAppHandler() {
+    invoke("receive_app_object");
+  }
+
+  async function callRustState() {
+    await invoke("use_state");
+
+    // console.log(win);
+    // console.log(appWindow);
+    // WebviewWindow.getByLabel("main");
+    // emit("test-event", {message: "emit"});
+    // await appWindow.emit('window-event', { message: "call window event" });
+  }  
+
+  return <div className="container">
       <h1>Welcome to Tauri!</h1>
 
       <div className="row">
         <span className="logos">
           <a href="https://nextjs.org" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={nextLogo}
-              className="logo next"
-              alt="Next logo"
-            />
+            <Image width={144} height={144} src={nextLogo} className="logo next" alt="Next logo" />
           </a>
         </span>
         <span className="logos">
           <a href="https://tauri.app" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={tauriLogo}
-              className="logo tauri"
-              alt="Tauri logo"
-            />
+            <Image width={144} height={144} src={tauriLogo} className="logo tauri" alt="Tauri logo" />
           </a>
         </span>
         <span className="logos">
           <a href="https://reactjs.org" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={reactLogo}
-              className="logo react"
-              alt="React logo"
-            />
+            <Image width={144} height={144} src={reactLogo} className="logo react" alt="React logo" />
           </a>
         </span>
       </div>
@@ -58,20 +94,34 @@ function App() {
 
       <div className="row">
         <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
+          <input id="greet-input" onChange={e => setName(e.currentTarget.value)} placeholder="Enter a name..." />
           <button type="button" onClick={() => greet()}>
             Greet
           </button>
         </div>
       </div>
-
-      <p>{greetMsg}</p>
-    </div>
-  );
+      <button type="button" onClick={() => resultTest("test_return_result_err")}>
+        result fail
+      </button>
+      <button type="button" onClick={() => resultTest("test_return_result_ok")}>
+        result success
+      </button>
+      <button type="button" onClick={() => callAsyncFunction()}>
+        async function
+      </button>
+      <button type="button" onClick={() => passWindow()}>
+        pass window
+      </button>
+      <button type="button" onClick={() => callRustState()}>
+        use state
+      </button>
+      <button type="button" onClick={() => passAppHandler()}>
+        pass AppHandler
+      </button>
+      <p>
+        {greetMsg}
+      </p>
+    </div>;
 }
 
 export default App;
